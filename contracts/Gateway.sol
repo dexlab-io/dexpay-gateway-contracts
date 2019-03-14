cffaf5c768acdab872bec4e9dea6c8cbbd6bf408
alexintosh <alessio.d@gmail.com>
2019-03-14 14:51:00 +0100

    Adding payWithEth() function

--- contracts/Gateway.sol
+++ contracts/Gateway.sol
@@ -37,6 +37,28 @@ contract Gateway is Initializable, Ownable {
         return true;
     }
 
+    function payWithEth(address seller, uint _orderId, uint256 amount, bool _autoEx) public payable returns  (bool success){
+        require(seller != address(0), "Seller is an empty address"); 
+        require(msg.value > 0 && msg.value == amount, "msg.value doesn not match amount");
+
+        bool shouldExchange = false;
+        if(_autoEx == true) {
+            shouldExchange = true;
+        }
+
+        if(shouldExchange == true) {
+            require(X.tradeKyber.value(msg.value)(X.ETHToken, amount, X.DAItoken, seller), "Exchange failed");
+        } else {
+            require(seller.transfer(msg.value), "Transfer to seller failed");
+        }
+        
+        bytes32 data = keccak256(abi.encodePacked( seller,_orderId ) );
+            
+        payment[seller][_orderId] = PaymentObj(msg.sender, seller, X.ETHToken, amount, data, true);
+        emit ProofOfPayment(msg.sender, seller, GOToken, amount, data);
+        return true;
+    }
+
     function () external payable {
 
     }
