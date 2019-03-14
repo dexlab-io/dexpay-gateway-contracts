10fe33adfdc6ea8d2b38e9867c2f15ffe96f9b90
alexintosh <alessio.d@gmail.com>
2019-03-14 14:16:02 +0100

    Adding a first simple version of Dex aggregator

---
+++ contracts/Dex.sol
@@ -0,0 +1,146 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-eth/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-eth/contracts/ownership/Ownable.sol";
+import "zos-lib/contracts/Initializable.sol";
+import './KyberNetworkI.sol';
+
+contract GlobalVar {
+    /**
+    * Address Exchanges
+    */
+    address public KyberAddress = 0x818E6FECD516Ecc3849DAf6845e3EC868087B755; // KN Proxy, same for ropsten and mainnet
+    address public ED_contract = 0x8d12A197cB00D4747a1fe03395095ce2A5CC6819;
+    address public ED_contract_ropsten = 0x228344536a03C0910Fb8Be9C2755c1a0bA6f89E1;
+    
+    address public ETHToken = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address public OMGtoken = 0x4BFBa4a8F28755Cb2061c413459EE562c6B9c51b;
+    address public DAItoken = 0xaD6D458402F60fD3Bd25163575031ACDce07538D;
+    address public BATtoken = 0xDb0040451F373949A4Be60dcd7b6B8D6E42658B6;
+    address public KNCtoken = 0x4E470dc7321E84CA96FcAEDD0C8aBCebbAEB68C6;
+}
+
+contract Dex is Initializable, Ownable, GlobalVar {
+    uint256 public serviceFee;
+    address public KyberAddress;
+    KyberNetworkI private kyberExchange;
+
+    function initialize(address KyberAddress) public initializer {
+        serviceFee = 0;
+        kyberExchange = KyberNetworkI(KyberAddress);
+
+        //Preapprove Tokens
+        ERC20Interface OMGtkn = IERC20(OMGtoken);
+        OMGtkn.approve(KyberAddress, 2**256 - 1);
+        
+        ERC20Interface KNCtkn = IERC20(KNCtoken);
+        KNCtkn.approve(KyberAddress, 2**256 - 1);
+        
+        ERC20Interface BATtkn = IERC20(BATtoken);
+        BATtkn.approve(KyberAddress, 2**256 - 1);
+        
+        ERC20Interface DAItkn = IERC20(DAItoken);
+        DAItkn.approve(KyberAddress, 2**256 - 1);
+    }
+
+    function approve(ERC20Interface erc20, address spender, uint tokens) public onlyOwner returns (bool success) {
+        require(erc20.approve(spender, tokens), "Token Aprove aborted");
+        return true;
+    }
+
+    function withdraw() public onlyOwner returns (bool success) {
+        require(address(this).balance > 0, "Balance is zero");
+        msg.sender.transfer(address(this).balance);
+        return true;
+    }
+
+    function withdrawTokens(IERC20 token) public onlyOwner returns (bool success) {
+        uint256 balance = token.balanceOf(this);
+
+        // Double checking
+        require(balance > 0, "Balance is zero");
+        require(token.transfer(msg.sender, balance), "Token transfer aborted");
+        return true;
+    }
+
+    /**
+        * Instant super simple trading with kyber
+        * dest is the only parameter, it represent the token to buy
+        * because of minConversionRate=1, trade will execute according to market price in the time of the transaction confirmation.
+        * walletId is null here
+
+        Usecases:
+
+        Send ETH from any wallet to omg.gdepprotocol.eth to convert the ETH to OMG through a fallback function. This could use a relayer broker contract to inject minimum values and prevent frontrunning.
+        Converting OMG to ETH by sending OMG to omg.uniswap.eth, and converting OMG to KNC by sending OMG to knc.uniswap.eth could be done using a trusted system of off-chain event watchers.
+        Send ETH from any wallet to OMG.alexintoth.gdepprotocol.eth to convert that ETH to OMG and transfer it to alexintoth.
+    */
+    function tradeKyber(
+        address source, //0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee, // eth token in kyber
+        uint srcAmount,
+        address dest,
+        address recepient
+    )
+        public
+        payable
+        returns (uint256 swappedAmount)
+    {
+        
+        /**
+        * If we are receiving ether, than 
+        */
+        uint amountToSend;
+        if (source == ETHToken) {
+            amountToSend = msg.value;
+        } else {
+            /*
+            * It's not ether, we need to transfer the token from the user to this contract
+            * This will assume the user has called approve(source) with this contract address
+            * Otherwise function will trow
+            */
+            amountToSend = srcAmount;
+            IERC20 tokenFunctions = IERC20(source);
+            //TODO add require
+            tokenFunctions.transferFrom(msg.sender, address(this), amountToSend);
+        }
+        
+        
+        uint256 actualDestAmount = kyberExchange.trade.value(amountToSend)(
+            source, // eth token in kyber
+            amountToSend, //amount of tokens to convert. If sending ETH must be equal to msg.value. Otherwise, must not be higher than user token allowance to kyber network contract address.
+            dest, 
+            this,
+            2**256 - 1, //maxDestAmount: maximum destination amount. The actual converted amount will be the minimum of srcAmount and required amount to get maxDestAmount of dest tokens. For an exchange application, we recommend to set it to MAX_UINT (i.e., 2**256 - 1).
+            1, //minConversionRate: the minimal conversion rate. If the current rate is too high, then the transaction is reverted. For an exchange application this value can be set according to the priceSlippage return value of getExpectedRate. However, in this case, the execution of the transaction is not guaranteed in case big changes in market price happens before the confirmation of the transaction. A value of 1 will execute the trade according to market price in the time of the transaction confirmation.
+            0
+        );
+        
+        
+        /**
+        * Fee calculation
+        */
+        uint256 transferAmount;
+        if (serviceFee > 0) {
+            uint256 feeToSub = (actualDestAmount * serviceFee) / (1 ether);
+
+            transferAmount = actualDestAmount - feeToSub;
+        }
+        else {
+            transferAmount = actualDestAmount;
+        }
+        
+        address deliverTo;
+        if( recepient == address(0) ) {
+            deliverTo = msg.sender;
+        } else {
+            deliverTo = recepient;
+        }
+
+        /**
+        * Tokens are send back to the users
+        */
+        require(IERC20(dest).transfer(deliverTo, transferAmount));
+        return transferAmount;
+    }
+
+}
